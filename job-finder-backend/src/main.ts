dd13741780af97ce5cff2f0264a804aaff5beb03
ArekKuna <arkadiusz.kuna@jutromedical.com>
2025-02-28 10:11:14 +0100

    small fix on app port select

--- job-finder-backend/src/main.ts
+++ job-finder-backend/src/main.ts
@@ -1,9 +1,13 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from 'app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const appPort = configService.get<number>('APP_PORT');
+
+  await app.listen(appPort || 4000);
 }
 bootstrap();
