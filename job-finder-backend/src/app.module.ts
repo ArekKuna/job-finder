cd0141821bf228163b3ce43543032c4b57c03c55
ArekKuna <arkadiusz.kuna@jutromedical.com>
2025-02-28 09:58:36 +0100

    add nest configuration for test and dev environments

--- job-finder-backend/src/app.module.ts
+++ job-finder-backend/src/app.module.ts
@@ -1,31 +1,19 @@
 import { Module, ValidationPipe } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { APP_PIPE } from '@nestjs/core';
 import { JwtModule } from '@nestjs/jwt';
 import { UsersModule } from 'modules/users/users.module';
-import { User } from 'modules/users/user.entity';
 import { AuthModule } from 'modules/auth/auth.module';
+import { DatabaseConfig } from 'config/database.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: process.env.NODE_ENV === 'test' ? '.env.test' : '.env',
     }),
     TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('DB_HOST'),
-        port: configService.get('DB_PORT'),
-        username: configService.get('DB_USER'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_NAME'),
-        synchronize: true,
-        entities: [User],
-      }),
-      inject: [ConfigService],
+      useClass: DatabaseConfig,
     }),
     UsersModule,
     AuthModule,
