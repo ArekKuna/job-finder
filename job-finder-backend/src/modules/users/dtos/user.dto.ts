b05d7ff1134c72ba46be3bc9da6f85cb79944df4
ArekKuna <arkadiusz.kuna@jutromedical.com>
2025-03-02 20:44:39 +0100

    finish users signup e2e tests

--- job-finder-backend/src/modules/users/dtos/user.dto.ts
+++ job-finder-backend/src/modules/users/dtos/user.dto.ts
@@ -7,10 +7,10 @@ export class UserDto {
   @IsString()
   @MinLength(8, { message: 'Password must be at least 8 characters long' })
   @Matches(/[a-z]/, {
-    message: 'Password must contain at least one lowercase letter',
+    message: 'Password must contain at least one lowercase character',
   })
   @Matches(/[A-Z]/, {
-    message: 'Password must contain at least one uppercase letter',
+    message: 'Password must contain at least one uppercase character',
   })
   @Matches(/[\W_]/, {
     message: 'Password must contain at least one special character',
