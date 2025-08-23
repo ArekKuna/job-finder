2a4810e155f13733b3dc8ef833bc35c2ef1125ee
ArekKuna <91729521+ArekKuna@users.noreply.github.com>
2025-08-23 18:18:15 +0200

    add animated hiding header (#20)

--- job-finder-frontend/src/components/Layout/Header/index.tsx
+++ job-finder-frontend/src/components/Layout/Header/index.tsx
@@ -1,14 +1,37 @@
 import { MobileHeader } from "components/Layout/Header/MobileHeader";
 import { JobFinderLogo } from "components/Layout/JobFinderLogo/JobFinderLogo";
 import { Link } from "react-router-dom";
+import { motion, useMotionValueEvent, useScroll } from "framer-motion";
+import { useRef, useState } from "react";
 
 export const Header = () => {
+  const [hidden, setHidden] = useState(false);
+
+  const { scrollY } = useScroll();
+  const lastScrollY = useRef(0);
+
+  useMotionValueEvent(scrollY, "change", (currentY) => {
+    if (currentY > lastScrollY.current && currentY > 50) {
+      setHidden(true);
+    } else {
+      setHidden(false);
+    }
+    lastScrollY.current = currentY;
+  });
+
   return (
-    <header className="w-full flex justify-between p-4 items-center">
+    <motion.header
+      initial={false}
+      animate={{
+        y: hidden ? "-100%" : "0%",
+      }}
+      transition={{ duration: 0.1 }} // ease-in-out}
+      className="fixed top-0 z-50 w-full flex justify-between p-2 items-center bg-white shadow transition-all"
+    >
       <Link to="/">
         <JobFinderLogo />
       </Link>
       <MobileHeader />
-    </header>
+    </motion.header>
   );
 };
--- job-finder-frontend/src/components/UI/Drawer/index.tsx
+++ job-finder-frontend/src/components/UI/Drawer/index.tsx
@@ -12,20 +12,20 @@ export const Drawer = ({ isOpen, children, onClose }: Props) => {
       {isOpen && (
         <>
           <motion.div
-            className="fixed inset-0 bg-black/80 z-40"
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
             transition={{ duration: 0.3 }}
             onClick={onClose}
+            className="fixed inset-0 bg-black/80 z-40"
           />
 
           <motion.div
-            className="fixed top-0 right-0 h-full w-3/4 bg-white shadow-lg z-50"
             initial={{ x: "100%" }}
             animate={{ x: 0 }}
             exit={{ x: "100%" }}
             transition={{ duration: 0.3 }}
+            className="fixed top-0 right-0 h-full w-3/4 bg-white shadow-lg z-50"
           >
             {children}
           </motion.div>
