34f1d88883ad15126d970b134d564a23df46aa66
ArekKuna <91729521+ArekKuna@users.noreply.github.com>
2025-08-23 17:32:25 +0200

    routes clean up after redesign (#19)

--- job-finder-frontend/src/views/Router.tsx
+++ job-finder-frontend/src/views/Router.tsx
@@ -1,10 +1,15 @@
 import { RouteWrapper } from "components/RouteWrapper/RouteWrapper";
 import { BrowserRouter, Route, Routes } from "react-router";
-import { EmployeeRegistration } from "views/EmployeeRegistration/EmployeeRegistration";
+import { About } from "views/About";
+import { Companies } from "views/Companies";
+import { EmployeeRegistration } from "views/EmployeeRegistration";
+import { EmployerRegistration } from "views/EmployerRegistration";
 import { GetStartedPage } from "views/GetStarted";
-import { Home } from "views/Home/Home";
-import { Login } from "views/Login/Login";
+import { Home } from "views/Home";
+import { JobBoard } from "views/JobBoard";
+import { Login } from "views/Login";
 import { MyProfile } from "views/MyProfile";
+import { UserSettings } from "views/UserSettings";
 
 export const AppRoutes = () => {
   return (
@@ -12,13 +17,21 @@ export const AppRoutes = () => {
       <RouteWrapper>
         <Routes>
           <Route index element={<Home />} />
+          <Route path="/job-board" element={<JobBoard />} />
+          <Route path="/companies" element={<Companies />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/get-started" element={<GetStartedPage />} />
           <Route
             path="/employee-registration"
             element={<EmployeeRegistration />}
           />
-          <Route path="/get-started" element={<GetStartedPage />} />
-          <Route path="/login" element={<Login />} />
+          <Route
+            path="/employer-registration"
+            element={<EmployerRegistration />}
+          />
           <Route path="/profile" element={<MyProfile />} />
+          <Route path="/settings" element={<UserSettings />} />
         </Routes>
       </RouteWrapper>
     </BrowserRouter>
