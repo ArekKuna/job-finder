6c48402514ad431901b0372c344d492df8726f9e
ArekKuna <arkadiusz.kuna@jutromedical.com>
2025-08-26 13:37:21 +0200

    add global header height padding

--- job-finder-frontend/src/components/Layout/index.tsx
+++ job-finder-frontend/src/components/Layout/index.tsx
@@ -12,7 +12,7 @@ export const Layout = ({ children }: Props) => {
   return (
     <>
       <Header />
-      <div className="min-h-[2000px] pt-[72px]">{children}</div>
+      <div className="min-h-[2000px] pt-[74px]">{children}</div>
       <Footer />
     </>
   );
